49ff31038be5012ef3039bd8f69c0dae724db42d
TMYuan <piews482zt@gmail.com>
2018-01-13 13:41:32 +0800

    skip connection

--- Final/model_test.py
+++ Final/model_test.py
@@ -43,13 +43,17 @@ class PSPUpsample(nn.Module):
 class PSPNet(nn.Module):
     def __init__(self, densenet, n_classes=1, sizes=(2, 3, 6, 8), psp_size=1024):
         super(PSPNet, self).__init__()
-        self.features = densenet.features
+        self.feature1 = nn.Sequential(*list(densenet.features.children())[0:4])
+        self.feature2 = nn.Sequential(*list(densenet.features.children())[4:6])
+        self.feature3 = nn.Sequential(*list(densenet.features.children())[6:8])
+        self.feature4 = nn.Sequential(*list(densenet.features.children())[8:10])
+        self.feature5 = nn.Sequential(*list(densenet.features.children())[10:12])
         
         self.psp = PSPModule(psp_size, 512, sizes)
         self.up_1 = PSPUpsample(512, 256)
-        self.up_2 = PSPUpsample(256, 64)
-        self.up_3 = PSPUpsample(64, 32)
-        self.up_4 = PSPUpsample(32, 16)
+        self.up_2 = PSPUpsample(256 + 256, 64)
+        self.up_3 = PSPUpsample(128 + 64, 32)
+        self.up_4 = PSPUpsample(32 + 64, 16)
         self.up_5 = PSPUpsample(16, 8)
         
         self.final = nn.Sequential(
@@ -57,15 +61,20 @@ class PSPNet(nn.Module):
             nn.Sigmoid()
         )
         
-        for p in self.features.parameters():
-            p.requires_grad = False
+        #for p in self.features.parameters():
+        #    p.requires_grad = False
             
     def forward(self, x):
-        f = self.features(x)
-        p = self.psp(f)
-        p = self.up_1(p)
-        p = self.up_2(p)
-        p = self.up_3(p)
+        f1 = self.feature1(x)
+        f2 = self.feature2(f1)
+        f3 = self.feature3(f2)
+        f4 = self.feature4(f3)
+        f5 = self.feature5(f4)
+        
+        p = self.psp(f5)
+        p = torch.cat([self.up_1(p), f3], 1)
+        p = torch.cat([self.up_2(p), f2], 1)
+        p = torch.cat([self.up_3(p), f1], 1)
         p = self.up_4(p)
         p = self.up_5(p)
         
